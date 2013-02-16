5a404dc58af6c9c27faa21e41b9f30ac48c7bb70
bender2012 <netant@inbox.ru>
2013-02-16 21:59:51 +0200

    Created maven project

---
+++ src/main/java/com/epam/vm/app/Application.java
@@ -0,0 +1,18 @@
+package com.epam.vm.app;
+
+public class Application {
+
+	public Application() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+
+	}
+
+}
